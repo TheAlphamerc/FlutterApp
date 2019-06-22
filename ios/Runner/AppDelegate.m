f8590c1c83d5af8a2e5ae23066ff01827b49e7ed
sonukritivity <sonu.sharma@kritivity.com>
2019-06-22 18:50:34 +0530

    Adding ios native code to get battery level

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -1,13 +1,39 @@
 #include "AppDelegate.h"
 #include "GeneratedPluginRegistrant.h"
-
+#import <Flutter/Flutter.h>
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
   [GeneratedPluginRegistrant registerWithRegistry:self];
   // Override point for customization after application launch.
+  FlutterViewController* controller = (FlutterViewController*)self.window.rootViewController;
+  FlutterMethodChannel* batteryChannel = [FlutterMethodChannel methodChannelWithName: @"flutter-app.com/battery" binaryMessenger: controller];
+  [batteryChannel setMethodCallHandler: ^(FlutterMethodCall* call, FlutterResult result) {
+    if ([@"getBatteryLevel" isEqualToString:call.method]) {
+      int batteryLevel = [self getBatteryLevel];
+
+      if (batteryLevel == -1) {
+        result([FlutterError errorWithCode:@"UNAVAILABLE" message:@"Battery info not available." details:nil]);
+      } else {
+        result(@(batteryLevel));
+      }
+    } else {
+      result(FlutterMethodNotImplemented);
+    }
+  }];
+  
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
+}
+
+- (int)getBatteryLevel {
+  UIDevice* device = UIDevice.currentDevice;
+  device.batteryMonitoringEnabled = YES;
+  if (device.batteryState == UIDeviceBatteryStateUnknown) {
+    return -1;
+  } else {
+    return (int)(device.batteryLevel * 100);
+  }
 }
 
 @end
